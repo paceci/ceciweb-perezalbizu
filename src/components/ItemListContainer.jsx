9362a1647f6d1684b503ca18ff60fed02e05ecd7
Ceci Perez Albizu <pa.ceci@gmail.com>
2022-07-21 16:50:44 -0300

    modificaciones entrega parcial

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,39 +1,50 @@
 import React, { useState, useEffect } from "react";
 import ItemList from "./ItemList";
-import categorias from "./utils/categorias";
+import macetas from "./utils/macetas";
 import { useParams } from "react-router-dom";
 
 const ItemListContainer = ({greeting}) => {
-
-    const { categoria } = useParams();
-    console.log(categoria);
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState([]);
+  const { categoria } = useParams();
+  console.log(categoria);
     
-    let [items, setItems] = useState([]);
+  // let [items, setItems] = useState([]);
 
     const promise = new Promise ((resolve) => {
-        setTimeout(() => resolve(categorias), 2000);
+        setTimeout(() => resolve(macetas), 2000);
      });
 
      useEffect(() => {
-        promise.then((res) => {
-          const products = res;
-          if (categoria) {
-            setItems(products.filter((product) => product.category === categoria));
-          } else {
-            setItems(products);
-          }
-        });
-      }, [categoria]);
+      setIsLoading(true);
+      
+      promise
+      .then((products) => {
+        if (categoria) {
+          setItems(
+            products.filter((product) => product.categoria === categoria)
+          );
+        } else {
+          setItems(products);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [categoria]);
 
     return ( 
         <>
-        <div className="container-fluid">
+        {isLoading ? (
+          <p class="slogan">Estamos buscando los productos ...</p>
+        ) : (
+          <div className="container-fluid">
             <p class="slogan">{greeting}</p>
             <ItemList items={items}/>
-        </div>
-        </>
-        
+          </div>
+        )}
+      </>   
     );
-}
+};
  
 export default ItemListContainer;
